c7f833f403be0d89afc973bd16813309cac38c9a
Andrea Milanta <andrea.milanta@gmail.com>
2019-04-21 22:18:22 +0200

    Added SendEventA as example

---
+++ src/examples/Headers/SendEventA.h
@@ -0,0 +1,16 @@
+#ifndef SENDEVENTA_H
+#define SENDEVENTA_H
+
+// Standard Include
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <strsafe.h>
+
+// SimConnect
+#include "SimConnect.h"
+
+// Prototypes
+void testSendEvent();
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "ClientEvent.h"
+#include "SendEventA.h"
 
 int __cdecl _tmain(int argc, _TCHAR* argv[])
 {
     printf("Hello World");
-    testClientEvents();
+    // testClientEvents();
+    testSendEvent();
 
 	return 0;
 }
